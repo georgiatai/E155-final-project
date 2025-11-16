0d95cdabf62bb0ddb7e3cbbc019b6a295e6b1f3b
juliacygong <jgong@g.hmc.edu>
2025-11-15 20:25:09 -0800

    synthesis edits

--- fpga/src/fftdec.sv
+++ fpga/src/fftdec.sv
@@ -6,31 +6,29 @@ module fftdec #(parameter BIT_WIDTH = 16, N = 9, FFT_SIZE = 512, FS = 48000)
                (input logic clk, reset,
                 input logic fft_done, 
                 input logic [BIT_WIDTH * 2 - 1:0] fft_result,
-                output logic [BIT_WIDTH:0] frequency
+                output logic [BIT_WIDTH:0] frequency,
                 output logic note_dec);
 
-logic [BIT_WIDTH - 1:0] magnitude_sq, magnitude_max, real, img;
+logic [BIT_WIDTH - 1:0] magnitude_sq, magnitude_max, real_v, img;
 logic [BIT_WIDTH - 1:0] real_sq, img_sq;
 
 // fft output index
 logic [N - 1:0] k, k_max;
 
-assign real = [2 * BIT_WIDTH - 1:BIT_WIDTH];
-assign img = [BIT_WIDTH - 1:0];
+assign real_v = fft_result[2 * BIT_WIDTH - 1:BIT_WIDTH];
+assign img = fft_result[BIT_WIDTH - 1:0];
 
 multiply #(.BIT_WIDTH(BIT_WIDTH))
-  mag_real(.a(real)
-           .b(real)
+  mag_real(.a(real_v),
+           .b(real_v),
            .prod_trunc(real_sq));
 
 multiply #(.BIT_WIDTH(BIT_WIDTH))
-   mag_img(.a(img)
-           .b(img)
+   mag_img(.a(img),
+           .b(img),
            .prod_trunc(img_sq));
 
-assign magnitude_sq = real_sq + img_sq;
-
-always_ff @(posedge clk, reset) begin
+always_ff @(posedge clk) begin
     if (~reset) begin
         magnitude_sq <= 0;
         magnitude_max <= 0;
@@ -40,6 +38,7 @@ always_ff @(posedge clk, reset) begin
         note_dec <= 0;
     end
     else if (fft_done) begin 
+		magnitude_sq <= real_sq + img_sq;
         if (magnitude_sq > magnitude_max) begin
             magnitude_max <= magnitude_sq;
             k_max <= k;
